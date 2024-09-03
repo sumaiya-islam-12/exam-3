e58f2302a15b009789788f5bc556a34933d730ae
Your Name <sumaiyaislam9997@gmail.com>
2024-09-03 11:52:36 +0600

    first commit

---
+++ script.js
@@ -0,0 +1,23 @@
+
+function nickname(){
+    document.getElementById("name").innerHTML ="My nickname is Shanti"
+}
+
+function myimage() {
+   document.getElementById('myimage').src='image/WhatsApp Image 2024-08-21 at 15.56.12_58af10a6.jpg'
+  }
+
+function image(){
+    document.getElementById('myimage').src='image/WhatsApp Image 2024-08-21 at 15.56.12_6258ad9a.jpg'
+}
+
+function mystyle(){
+   document.getElementById('style').style.color="green";
+}
+
+function hidetext(){
+    document.getElementById('hide').style.display='none'
+}
+function showtext(){
+    document.getElementById('show').style.display='block'
+}
